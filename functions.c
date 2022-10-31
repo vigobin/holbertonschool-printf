cdd9f0b1c2284f38bcd21794e825a1ee6451f045
vigobin <vgobin@outlook.com>
2022-10-31 08:35:39 -0700

    Updated to add function to write integer

--- functions.c
+++ functions.c
@@ -10,3 +10,40 @@ int put_char(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ * @str : string input.
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int length;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+		put_char(str[length]);
+	}
+	put_char('\n');
+}
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		put_char('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	put_char((num % 10) + '0');
+}
