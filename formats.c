b075cd131ff09916c221b34cbf0632dc73e49fd9
vigobin <vgobin@outlook.com>
2022-10-31 12:26:07 -0700

    added file for formats

---
+++ formats.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ *format_specifier - function that selects the correct format.
+ *@s: operator passed as argument to the program.
+ *Return: NULL for no match.
+ */
+
+int (*format_specifier(char *s))(char)
+{
+	int i;
+
+	op_t opsel[] = {
+		{"c", spec_c},
+		{"s", spec_s},
+		{"%", spec_perc},
+		{"d", spec_d},
+		{"i", spec_i},
+		{NULL, NULL}
+	};
+
+	i = 0;
+
+	while (opsel[i].op)
+	{
+		if (*(opsel[i].op) == *s)
+			return (opsel[i].f);
+		i++;
+	}
+	return (NULL);
+}
+
+int spec_c(char c)
+{
+	int i;
+
+	for (i = 0)
+}
