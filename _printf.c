39d1deb9cd46c806b25f56d259be55015b44526d
Dustin Davis <davisdustin328@gmail.com>
2022-10-31 11:05:49 -0700

    adding Functions, main.h and some test files

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - Function that produces output according to a format.
+ * @format: Character string composed of zero or more directives.
+ * Return: The number of characters printed, excluding the null byte.
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
---
+++ functions.c
@@ -0,0 +1,80 @@
+#include "main.h"
+
+/**
+ * put_char - writes the character c to output.
+ * @c: The character to print
+ * Return: On success 1.
+ */
+
+int put_char(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ * @str : string input.
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int length;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+		put_char(str[length]);
+	}
+	put_char('\n');
+}
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		put_char('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	put_char((num % 10) + '0');
+}
+
+/**
+ * print_strings - print varying amount of strings
+ * @separator: delimiter
+ * @n: amount of arguments in list
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+	char *s;
+
+	if (n > 0)
+	{
+		va_start(valist, n);
+		for (i = 1; i <= n; i++)
+		{
+			s = va_arg(valist, char *);
+			if (s == NULL)
+				_puts("(nil)");
+			else
+				_puts("%s", s);
+
+			if (i != n && separator != NULL)
+				_puts("%s", separator);
+		}
+		va_end(valist);
+	}
+	_puts("\n");
+}
