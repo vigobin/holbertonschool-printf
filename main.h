dc221be43b9d29d579378daa31f4a62cca39ad04
Dustin Davis <davisdustin328@gmail.com>
2022-10-31 11:17:48 -0700

    added _puts prototype

--- main.h
+++ main.h
@@ -5,7 +5,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
 int put_char(char c);
 int _printf(const char *format, ...);
+void _puts(char *str)
 
 #endif
