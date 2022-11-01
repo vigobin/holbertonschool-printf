96953e697538c354dd47ec21a6b62c6797a14509
vigobin <vgobin@outlook.com>
2022-11-01 07:34:47 -0700

    updated main header prototypes

--- main.h
+++ main.h
@@ -22,5 +22,10 @@ int put_char(char c);
 int _printf(const char *format, ...);
 void _puts(char *str);
 void print_number(int n);
-	
+int spec_c(va_list c);
+int spec_s(va_list s);
+int spec_perc(va_list p);
+int spec_d(va_list d);
+int spec_i(va_list i);
+
 #endif
