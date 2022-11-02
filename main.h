ba1abb586881653128bb44918cfd7e0c3657be86
Dustin Davis <davisdustin328@gmail.com>
2022-11-01 21:04:49 -0700

    adding finishing changes

--- main.h
+++ main.h
@@ -6,7 +6,6 @@
 #include <unistd.h>
 #include <stdio.h>
 
-
 /**
  * struct op - Struct op
  *
@@ -17,12 +16,19 @@
 typedef struct op
 {
 	char *op;
-	char *(*f)(va_list);
+	char *(*f)(va_list args);
 } op_t;
 
-int put_char(char c);
 int _printf(const char *format, ...);
+int (*format_specifier(char *f))(va_list args);
+int spec_c(va_list args);
+int spec_s(va_list args);
+int spec_perc(va_list args);
+int spec_id(va_list args);
+int _re_integer(int ab);
+int put_char(char c);
 void _puts(char *str);
 void print_number(int n);
+void print_strings (const unsigned int n, ...);
 
 #endif
